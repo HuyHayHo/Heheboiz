3f99424147d88105097ab38fa2a106d5bea74d81
HuyHayHo <92163299+HuyHayHo@users.noreply.github.com>
2023-05-05 22:39:15 +0700

    Delete 111 directory

--- 111/oldest_student.h
+++
@@ -1,6 +0,0 @@
-#ifndef OLDEST_STUDENT_H
-#define OLDEST_STUDENT_H
-
-struct student findOldestStudent(struct student *students, int numOfStudents);
-
-#endif
--- 111/youngest_student.h
+++
@@ -1,6 +0,0 @@
-#ifndef YOUNGEST_STUDENT_H
-#define YOUNGEST_STUDENT_H
-
-struct student findYoungestStudent(struct student *students, int numOfStudents);
-
-#endif
